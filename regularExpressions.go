e306496f5609d167559db8146bec7821025a1b9e
unknown <g00340481@gmit.ie>
2017-11-15 22:18:11 +0000

    added function, added code

--- regularExpressions.go
+++ regularExpressions.go
@@ -8,15 +8,36 @@ import (
 	"time"
 )
 
-func main() {
-	//rand.Seed(42) //try changing this number
-	//rand.Seed(time.Now().UTC().UnixNano())
-	rand.Seed(time.Now().Unix())
-
+func ElizaResponse(input string) string { //input string, output string
 	answers := []string{
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?",
 	}
-	fmt.Println("Eliza replies:", answers[rand.Intn(len(answers))])
+	return answers[rand.Intn(len(answers))]
+}
+
+func main() {
+	//rand.Seed(42) //rovided seed value to initialize the default Source to a deterministic state
+	//rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().Unix()) //seed rand with Unix time
+
+	/* answers := []string{
+		"I’m not sure what you’re trying to say. Could you explain it to me?",
+		"How does that make you feel?",
+		"Why do you say that?",
+	}
+	fmt.Println("Eliza replies:", answers[rand.Intn(len(answers))]) */
+
+	fmt.Println("People say I look like both my mother and father.")
+	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	fmt.Println("Father was a teacher.")
+	fmt.Println(ElizaResponse("Father was a teacher."))
+	fmt.Println("I was my father’s favourite.")
+	fmt.Println(ElizaResponse("I was my father’s favourite."))
+	fmt.Println("I’m looking forward to the weekend.")
+	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
+	fmt.Println("My grandfather was French!")
+	fmt.Println(ElizaResponse("My grandfather was French!"))
+
 } //main
