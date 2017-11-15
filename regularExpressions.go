e9711682b99b2395858460b0774ba2838daf5d77
unknown <g00340481@gmit.ie>
2017-11-15 23:10:26 +0000

    added code, changed reg.exp

--- regularExpressions.go
+++ regularExpressions.go
@@ -1,14 +1,27 @@
-//Adapted from https://golang.org/pkg/math/rand/
-//Adapted from https://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
+//Adapted from & researched:
+//https://golang.org/pkg/math/rand/
+//https://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
+//https://techterms.com/definition/wildcard
+//https://github.com/google/re2/wiki/Syntax
+
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"regexp" //Package regexp implements regular expression search
 	"time"
 )
 
 func ElizaResponse(input string) string { //input string, output string
+
+	//MatchString checks whether a textual regular expression matches a string
+	//func MatchString(pattern string, s string) (matched bool, err error)
+	//if matched, _ := regexp.MatchString(".*father.*", input); matched { //.* matches zero or more characters (any) before and after father
+	if matched, _ := regexp.MatchString(".*[Ff]ather.*", input); matched { //[]inside square brackets = anything...logical or
+		return "Why don’t you tell me more about your father?"
+	}
+
 	answers := []string{
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
@@ -31,13 +44,22 @@ func main() {
 
 	fmt.Println("People say I look like both my mother and father.")
 	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	fmt.Println()
+
 	fmt.Println("Father was a teacher.")
 	fmt.Println(ElizaResponse("Father was a teacher."))
+	fmt.Println()
+
 	fmt.Println("I was my father’s favourite.")
 	fmt.Println(ElizaResponse("I was my father’s favourite."))
+	fmt.Println()
+
 	fmt.Println("I’m looking forward to the weekend.")
 	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
+	fmt.Println()
+
 	fmt.Println("My grandfather was French!")
 	fmt.Println(ElizaResponse("My grandfather was French!"))
+	fmt.Println()
 
 } //main
